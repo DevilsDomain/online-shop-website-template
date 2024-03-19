24122bc8baaaccbc4fef6c8e15630a346d33691b
DevilsDomain <alaraerzincan@gmail.com>
2024-03-19 11:26:33 +0100

    vite config fixed, updated script tags

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,17 @@
-// import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 
-// export default defineConfig({
-//   // Specify the directory where Vite will look for your source files
-//   root: './src',
+export default defineConfig({
+  // Specify the directory where Vite will look for your source files
+  root: './',
 
-//   // Specify the directory where Vite will output the built files
-//   build: {
-//     outDir: '../dist',
-//   },
+  // Specify the directory where Vite will output the built files
+  build: {
+    outDir: './dist',
+  },
 
-//   // Enable or disable server features
-//   server: {
-//     // Configure the port for the development server
-//     port: 3000,
-//   },
-  
-//   // Configure how Vite treats different file types
-//   // For example, you can use plugins to handle CSS, HTML, and other file types
-//   plugins: [],
-
-//   // Configure Vite to serve static files
-//   // For example, you can specify additional directories to be served by the development server
-//   // This is useful for serving images, fonts, or other assets
-//   publicDir: '../',
-
-//   // Configure Vite to resolve imports from specific directories
-//   resolve: {
-//     alias: {
-//       // For example, you can alias the '@' symbol to the 'src' directory
-//       '@': '/src',
-//     },
-//   },
-// });
+  // Configure the development server
+  server: {
+    // Configure the port for the development server
+    port: 3000,
+  },
+});
