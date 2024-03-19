2a166e11db7de657cc386da3e1e18d39e2f6787a
DevilsDomain <alaraerzincan@gmail.com>
2024-03-19 11:03:02 +0100

    changed project structure, added relative paths

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,34 @@
-// vite.config.js
-export default {
-    // Your Vite configuration options go here
-  }
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   // Specify the directory where Vite will look for your source files
+//   root: './src',
+
+//   // Specify the directory where Vite will output the built files
+//   build: {
+//     outDir: '../dist',
+//   },
+
+//   // Enable or disable server features
+//   server: {
+//     // Configure the port for the development server
+//     port: 3000,
+//   },
   
+//   // Configure how Vite treats different file types
+//   // For example, you can use plugins to handle CSS, HTML, and other file types
+//   plugins: [],
+
+//   // Configure Vite to serve static files
+//   // For example, you can specify additional directories to be served by the development server
+//   // This is useful for serving images, fonts, or other assets
+//   publicDir: '../',
+
+//   // Configure Vite to resolve imports from specific directories
+//   resolve: {
+//     alias: {
+//       // For example, you can alias the '@' symbol to the 'src' directory
+//       '@': '/src',
+//     },
+//   },
+// });
